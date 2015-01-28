f8fbe97429c2072effd8ac7d97cbfa978172bd69
Steven Kryskalla <steve@commandiq.com>
2015-01-28 15:04:15 -0800

    tests: remove "ONLY_RUN_FAST_TESTS" flag and @slow decorator
    1. the @slow decorator is unused
    2. the logic seems backwards.. the code says: 'if ONLY_RUN_FAST_TESTS, then
       run the slow function, otherwise skip the function', that's opposite of what
       it should be doing
    
    So.. YAGNI. It can be added back (with proper logic) if it's really needed at
    some point.

--- run_tests.py
+++ run_tests.py
@@ -7,11 +7,10 @@ def main():
     if Popen(['redis-cli', 'info'], stdout=PIPE).wait() != 0:
         raise RuntimeError("Redis server is not running.")
 
-    env = "ONLY_RUN_FAST_TESTS=1" if "-f" not in sys.argv else ""
     pipe = "rg" if Popen(['which', 'rg'], stdout=PIPE).wait() == 0 else "cat"
 
     #run tests and gather output
-    p = Popen("%s /usr/bin/env python -m unittest discover -v -s tests %s" % (env, " ".join(sys.argv[1:])), shell=True, stdout=PIPE, stderr=STDOUT)
+    p = Popen("/usr/bin/env python -m unittest discover -v -s tests %s" % " ".join(sys.argv[1:]), shell=True, stdout=PIPE, stderr=STDOUT)
     exit_code = p.wait()
     (out, _) = p.communicate()
 
