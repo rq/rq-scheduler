597a31473e0442d2598086ecaba7b9a0b5c255b9
Igor Filatov <ia.filatov@gmail.com>
2015-07-18 13:18:56 +0300

    Configure logger (fix #69)

--- rq_scheduler/utils.py
+++ rq_scheduler/utils.py
@@ -1,5 +1,9 @@
 import calendar
 from datetime import datetime
+import logging
+
+from rq.utils import ColorizingStreamHandler
+
 
 # from_unix from times.from_unix()
 def from_unix(string):
@@ -11,3 +15,15 @@ def from_unix(string):
 def to_unix(dt):
     """Converts a datetime object to unixtime"""
     return calendar.timegm(dt.utctimetuple())
+
+
+def setup_loghandlers(level='INFO'):
+    logger = logging.getLogger('rq_scheduler.scheduler')
+    if not logger.handlers:
+        logger.setLevel(level)
+        formatter = logging.Formatter(fmt='%(asctime)s %(message)s',
+                                      datefmt='%H:%M:%S')
+        handler = ColorizingStreamHandler()
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
