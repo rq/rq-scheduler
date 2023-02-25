d22331711ad84f8f11e76a84f7dfde67b265e58e
Selwin Ong <selwin.ong@gmail.com>
2023-02-25 17:19:37 +0700

    Added Github Actions

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
